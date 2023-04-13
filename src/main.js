f184419bdb858b700b6695aabd604c5d6deaba33
FitiaHarena <randrianjafyfitiaharena@gmail.com>
2023-04-13 16:18:26 -0700

    Enable bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+//Load Bootstrap, BootstrapIcons and BootstrapVue
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 createApp(App).use(router).mount('#app')
